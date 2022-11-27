67500c6e87c266e84844034207867e96c15885f4
yueimar <yueimar97@gmail.com>
2022-11-27 15:05:12 -0500

    Great

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,53 +28,57 @@ const nuevoModulo = 21 % 0 === null;
 function devolverString(str) {
   // "Return" la string provista: s
   // Tu código:
-  return str
+  
+  return s;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
-  
+  // Tu código: 
+ return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if( x === y || y === x) return true;
+  else return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if(str1 === length(str2) || str2 === length(str1)) return true;
+  else return false;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90) return true;
+  else return false;
 }
 
 function mayorQueCincuenta(num) {
